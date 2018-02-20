e6e35d0130d022cf612afdd5d0156bf7e4437250
João Vitor Lessa <joao.barros@fatec.sp.gov.br>
2018-02-20 17:00:42 -0300

    Basemap changed

--- mapModules.js
+++ mapModules.js
@@ -1,13 +1,22 @@
 require([
+    "esri/basemaps",
     "esri/map", 
     "dojo/domReady!"
 ], 
-function(Map) {
+function(esriBasemaps, Map, domConstruct) {
+
+    //Customized Basemap
+    esriBasemaps.delorme = {
+        baseMapLayers: [{url: "https://services.arcgisonline.com/ArcGIS/rest/services/Specialty/DeLorme_World_Base_Map/MapServer"}
+        ],
+        thumbnailUrl: "https://www.example.com/images/thumbnail_2014-11-25_61051.png",
+        title: "Delorme"
+    };
 
     var map = new Map("map", {
       center: [-118, 34.5],
       zoom: 8,
-      basemap: "satellite"
-    });
+      basemap: "delorme"
+    }); 
 
   });
