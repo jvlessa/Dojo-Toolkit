85064c368090196a7299560e5ea883e2c861dbab
João Vitor Lessa <joaovitorlessa@gmail.com>
2018-05-23 23:26:11 -0300

    Dojo with Animation

--- hello-dojo/mydojo.js
+++ hello-dojo/mydojo.js
@@ -1,9 +1,20 @@
 require([
 			'dojo/dom',
-    		'dojo/dom-construct'
+    		'dojo/dom-construct',
+    		'dojo/fx',
+    		'dojo/domReady'
         ],
         //We call the modules here 
-  		function (dom, domConstruct) {
+  		function (dom, domConstruct, fx) {
             var greetingNode = dom.byId('greeting'); //Getting node by id
-            domConstruct.place('<em> Dojo!</em>', greetingNode); //Placing element after node
+            //domConstruct.place('<em> Dojo!</em>', greetingNode); //Placing element after node
+
+            greetingNode.innerHTML += ' from Dojo!';
+
+            //Visual effects
+            fx.slideTo({
+            	node: greetingNode,
+            	top: 100,
+            	left: 200
+            }).play();
         });
